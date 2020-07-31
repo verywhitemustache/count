b84aeab41e0add252ec59af830fee8b917d27591
Egor belousov <belousovegor33@gmail.com>
2020-08-01 00:01:48 +0300

    add changing transaction page's function

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 function countIncome () {
     var value = document.getElementById("amountOfIncome").value
     var select = document.getElementById("select-incomeItem").value
@@ -27,8 +26,9 @@ function countIncome () {
     var breadCrumbsContainer = document.getElementById("breadcrumbs-container")
     
     if (transactionSection.children.length > breadCrumbsContainer.children.length) {
-        var breadcrumb = document.createElement("div")
-        breadcrumb.className = "breadcrumbs-item button"
+        var breadcrumb = document.createElement("button")
+        breadcrumb.className = "breadcrumbs-item"
+        breadcrumb.setAttribute("onclick", 'whatButton(this);')
         breadcrumb.innerHTML = transactionSection.children.length
         breadCrumbsContainer.appendChild(breadcrumb)
     }
@@ -37,42 +37,44 @@ function countIncome () {
     
 }
 
-function countOutcome () {
+function countOutcome() {
     var value = document.getElementById("amountOfOutcome").value
     var select = document.getElementById("select-outcomeItem").value
-    
-    var transactionContainer = document.getElementById("page-container")
-    var transactionSection = document.getElementById("transaction-section")
+
     var transactionSectionForAdd = document.getElementById("transaction-section").lastElementChild
 
-    var breadCrumbsContainer = document.getElementById("breadcrumbs-container")
+    var transactionContainer = document.getElementById("page-container")
+    var transactionSection = document.getElementById("transaction-section")
 
-    var transaction = document.createElement("div")
-    
-    transaction.className = "page-item"
-    transaction.innerHTML = select + " " + value
-    transactionSectionForAdd.appendChild(transaction)
-    
-    
-    
-    if (transactionSectionForAdd.childNodes.length - 1   == 4) {
+    if (transactionSectionForAdd.childNodes.length + 1 > 4) {
         var previousSublings = document.querySelector(".page-container_active")
         previousSublings.classList.remove("page-container_active")
         var pageContainer = document.createElement("div")
         pageContainer.className = "page-container page-container_active"
         pageContainer.id = "page-container"
         transactionSection.appendChild(pageContainer)
-        
+    } else {
+        var transaction = document.createElement("div")
+        transaction.className = "page-item"
+        transaction.innerHTML = select + " " + value
+        transactionSectionForAdd.appendChild(transaction)
     }
-    
+
+
+    var breadCrumbsContainer = document.getElementById("breadcrumbs-container")
+
     if (transactionSection.children.length > breadCrumbsContainer.children.length) {
-        var breadcrumb = document.createElement("div")
-        breadcrumb.className = "breadcrumbs-item button"
+        var breadcrumb = document.createElement("button")
+        breadcrumb.className = "breadcrumbs-item"
+        breadcrumb.setAttribute("onclick", 'whatButton(this);')
         breadcrumb.innerHTML = transactionSection.children.length
         breadCrumbsContainer.appendChild(breadcrumb)
     }
 
+
+
 }
+
 
 
 function changeInputField () {
@@ -92,6 +94,12 @@ function changeInputField () {
 
 
 function whatButton(button) {
-    console.log("You clicked button with class " + button.className);
-    console.log("You clicked button with text " + button.innerText);
+    //console.log("You clicked button with class " + button.className);
+    //console.log("You clicked button with text " + button.innerText);
+    var buttonNumber = Number(button.innerHTML)
+    var transactionContainer = document.getElementById("transaction-section")
+    var previousSublings = document.querySelector(".page-container_active")
+    
+    previousSublings.classList.remove("page-container_active")
+    transactionContainer.children[buttonNumber - 1].classList.add("page-container_active")
 }
